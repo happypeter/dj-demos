ae454fa926ef09d3eced3cbb22dca2a33c84ffb9
Peter Wang <happypeter1983@gmail.com>
2017-09-01 16:40:10 +0800

    i

---
+++ react-demo/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './header.css'
+
+class header extends Component {
+  render () {
+    return (
+      <div className='header'>
+        header
+      </div>
+    )
+  }
+}
+
+export default header
