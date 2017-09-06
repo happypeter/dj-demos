bc980d544c3ce2ca08f2b1da71cf13ee8ef56582
Peter Wang <happypeter1983@gmail.com>
2017-09-06 09:13:39 +0800

    sidebar

---
+++ base/src/components/App/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './app.css'
+
+class App extends Component {
+
+  state = {
+    active: false
+  }
+
+  handleClick = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
+  render () {
+    return (
+      <div className='app'>
+        <div onClick={this.handleClick}
+          className={`button ${this.state.active&&'active'}`}>
+          click me
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ sidebar/src/components/App/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './app.css'
+
+class App extends Component {
+  state = {
+    open: true
+  }
+
+  handleClick = () => {
+    this.setState({
+      open: !this.state.open 
+    })
+  }
+  render () {
+    return (
+      <div className='app'>
+        <div onClick={this.handleClick}
+          className='burger'>
+
+        </div>
+        <div className={`sidebar ${this.state.open && 'show'}`}>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
