174369e92e75b25b512202784b05324b33023795
Peter Wang <happypeter1983@gmail.com>
2017-09-14 14:27:11 +0800

    dog6-form-submit

--- ten-dogs/src/components/Form/Form.js
+++ ten-dogs/src/components/Form/Form.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
 import './form.css'
+import axios from 'axios'
 
 class Form extends Component {
+
+  state = {
+    imgUrl: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      imgUrl: e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    console.log(this.state.imgUrl)
+    const imgUrl = this.state.imgUrl
+    axios.post('http://localhost:3008/dogs', { imgUrl })
+    .then(
+      res => {
+        console.log(res.data)
+      }
+    )
+    this.props.hideForm()
+  }
+
   render () {
     return (
       <div className='form'>
-        <input type='text' />
-        <button onClick={this.props.hideForm}>
+        <input value={this.state.imgUrl}
+        onChange={this.handleChange}
+        type='text' />
+        <button onClick={this.handleSubmit} >
         添加
         </button>
       </div>
