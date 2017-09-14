d7271aee712340e447fa0728257a1b327ead7081
Peter Wang <happypeter1983@gmail.com>
2017-09-14 10:20:02 +0800

    dog3-dog-list

---
+++ ten-dogs/src/components/DogIcon/DogIcon.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {
+  Link
+} from 'react-router-dom'
+
+class DogIcon extends Component {
+  render () {
+    const { dog } = this.props
+    return (
+      <div className='dog-icon'>
+        <Link to={`/dog/${dog.id}`}>
+           <div style={{
+               backgroundImage: `url(${dog.imgUrl})`
+             }}
+             className='dog-img'>
+           </div>
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default DogIcon
--- ten-dogs/src/components/Home/Home.js
+++ ten-dogs/src/components/Home/Home.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
+import './home.css'
+
+import DogIcon from '../DogIcon/DogIcon'
 
 class Home extends Component {
+  state = {
+    "dogs": [
+      {
+        "id": "1",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      },
+      {
+        "id": "2",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      },
+      {
+        "id": "3",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      },
+      {
+        "id": "4",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      },
+      {
+        "id": "5",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      },
+      {
+        "id": "6",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      },
+      {
+        "id": "7",
+        "imgUrl": "http://img0.imgtn.bdimg.com/it/u=4088053820,899019662&fm=27&gp=0.jpg"
+      }
+    ]
+  }
   render () {
+    const dogList = this.state.dogs.map(t => (
+      <DogIcon dog={t} />
+    ))
     return (
       <div className='home'>
-        Home
+        <div className='dog-list'>
+          {dogList}
+        </div>
       </div>
     )
   }
