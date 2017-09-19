0a43b3d9e823b32dd096b6184ebc6104195deace
Peter Wang <happypeter1983@gmail.com>
2017-09-19 09:35:48 +0800

    mv state to parent

--- simple-cart/src/components/App/App.js
+++ simple-cart/src/components/App/App.js
@@ -5,14 +5,61 @@ import Cart from '../Cart/Cart'
 
 class App extends Component {
 
+  state = {
+    productsInCart: [
+      {
+        id: '123',
+        name: '提拉米苏',
+        price: 12,
+        count: 0
+      },
+      {
+        id: '342',
+        name: '黑森林',
+        price: 13,
+        count: 0
+      }
+    ]
+  }
+
+  minus = (id) => {
+    console.log(id)
+    const newProductsInCart = this.state.productsInCart.map(t => {
+      if(id === t.id && t.count !== 0) {
+        return {...t, count: t.count - 1}
+      }
+      return t;
+    })
+    this.setState({
+      productsInCart: newProductsInCart
+    })
+  }
+
+  add = (id) => {
+    console.log(id)
+    const newProductsInCart = this.state.productsInCart.map(t => {
+      if(id === t.id) {
+        return {...t, count: t.count + 1}
+      }
+      return t;
+    })
+    this.setState({
+      productsInCart: newProductsInCart
+    })
+  }
+
   render () {
+    const { productsInCart } = this.state
     return (
       <div className='app'>
         <div className='products'>
            <Products />
         </div>
         <div className='cart'>
-           <Cart />
+           <Cart
+             add={this.add}
+             minus={this.minus}
+             productsInCart={productsInCart}/>
         </div>
       </div>
     )
--- simple-cart/src/components/Cart/Cart.js
+++ simple-cart/src/components/Cart/Cart.js
@@ -2,23 +2,6 @@ import React, { Component } from 'react'
 import './cart.css'
 
 class Cart extends Component {
-  state = {
-    total: 0,
-    productsInCart: [
-      {
-        id: '123',
-        name: '提拉米苏',
-        price: 12,
-        count: 0
-      },
-      {
-        id: '342',
-        name: '黑森林',
-        price: 13,
-        count: 0
-      }
-    ]
-  }
 
   calTotal = (products) => {
     const total = products.reduce((sum, t) => {
@@ -27,47 +10,19 @@ class Cart extends Component {
     return total
   }
 
-
-  minus = (id) => {
-    console.log(id)
-    const newProductsInCart = this.state.productsInCart.map(t => {
-      if(id === t.id && t.count !== 0) {
-        return {...t, count: t.count - 1}
-      }
-      return t;
-    })
-    this.setState({
-      productsInCart: newProductsInCart,
-      total: this.calTotal(newProductsInCart)
-    })
-  }
-
-  add = (id) => {
-    console.log(id)
-    const newProductsInCart = this.state.productsInCart.map(t => {
-      if(id === t.id) {
-        return {...t, count: t.count + 1}
-      }
-      return t;
-    })
-    this.setState({
-      productsInCart: newProductsInCart,
-      total: this.calTotal(newProductsInCart)
-    })
-  }
-
   render () {
-    const { total, productsInCart } = this.state
+    const { productsInCart } = this.props
+    const total = this.calTotal(productsInCart)
     const productList = productsInCart.map(t => (
       <div key={t.id} className='product-in-cart'>
         {t.name}--{t.price}
         <div className='count-wrap'>
-          <div onClick={() => this.minus(t.id)}
+          <div onClick={() => this.props.minus(t.id)}
           className='btn minus'>
             -
           </div>
           {t.count}
-          <div onClick={() => this.add(t.id)}
+          <div onClick={() => this.props.add(t.id)}
           className='btn add'>
             +
           </div>
