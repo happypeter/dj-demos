f5b95a90fa53161a07b212e66b50c6f57b2a6c7b
Peter Wang <happypeter1983@gmail.com>
2017-09-19 10:18:33 +0800

    cart-5-disabled

--- simple-cart/src/components/Products/Products.js
+++ simple-cart/src/components/Products/Products.js
@@ -7,23 +7,32 @@ class Product extends Component {
       {
         id: '123',
         name: '提拉米苏',
-        price: 12
+        price: 12,
+        isInCart: false
       },
       {
         id: '342',
         name: '黑森林',
-        price: 13
+        price: 13,
+        isInCart: false
       },
       {
         id: '456',
         name: '半熟芝士',
-        price: 15
+        price: 15,
+        isInCart: false
       }
     ]
   }
 
   handleClick = (id) => {
     console.log(id)
+    this.setState({
+      products: this.state.products.map(t => {
+        if(t.id === id) return { ...t, isInCart: true}
+        return t
+      })
+    })
     const product = this.state.products
                     .find(t => t.id === id)
     this.props.addToCart(product)
@@ -35,8 +44,10 @@ class Product extends Component {
       <div key={t.id} className='product'>
         <h1>{t.name}</h1>
         <p>{t.price}</p>
-        <button onClick={() => this.handleClick(t.id)}>
-          购买
+        <button disabled={t.isInCart}
+          className={t.isInCart && 'disabled'}
+          onClick={() => this.handleClick(t.id)}>
+          {t.isInCart ? '已购' : '购买'}
         </button>
       </div>
     ))
