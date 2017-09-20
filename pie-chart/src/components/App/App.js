63c4e598615eefb072779b2b8f64a14624105475
Peter Wang <happypeter1983@gmail.com>
2017-09-20 11:04:45 +0800

    chart-4-color

--- pie-chart/src/components/App/App.js
+++ pie-chart/src/components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './app.css'
-import {PieChart, Pie, Tooltip} from 'recharts'
+import {PieChart, Pie, Tooltip, Cell} from 'recharts'
 
 class App extends Component {
 
   render () {
     const data02 = [{name: '水分', value: 100}, {name: '蛋白质', value: 201},
                       {name: '碳水化物', value: 100}, {name: '淀粉', value: 111}]
+    const COLORS = ['#f4baba', '#e0cd84', '#91e1dd', '#a48ad4'];
 
     return (
       <div className='app'>
@@ -15,7 +16,13 @@ class App extends Component {
           <Pie data={data02} cx={150} cy={150}
             dataKey='value'
             innerRadius={60} outerRadius={90} fill="#82ca9d"
-            />
+            >
+            {
+              COLORS.map((t, i) => (
+                <Cell fill={t} />
+              ))
+            }
+          </Pie>
           <Tooltip/>
          </PieChart>
       </div>
