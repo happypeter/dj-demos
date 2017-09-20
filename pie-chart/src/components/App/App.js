8cf34b7092179fb8e498a10723d1c379d6731281
Peter Wang <happypeter1983@gmail.com>
2017-09-20 10:24:14 +0800

    chart-2-data

--- pie-chart/src/components/App/App.js
+++ pie-chart/src/components/App/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
 import './app.css'
-import {PieChart, Pie, Legend, Tooltip} from 'recharts'
+import {PieChart, Pie, Tooltip} from 'recharts'
 
 class App extends Component {
 
   render () {
-    const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
-                      {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
-                      {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
+    const data02 = [{name: '水分', value: 100}, {name: '蛋白质', value: 201},
+                      {name: '碳水化物', value: 100}, {name: '淀粉', value: 111}]
 
     return (
       <div className='app'>
       	<PieChart width={800} height={400}>
-          <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/>
+          <Pie data={data02} cx={500} cy={200}
+            dataKey='value'
+            innerRadius={40} outerRadius={80} fill="#82ca9d"
+            />
           <Tooltip/>
          </PieChart>
       </div>
