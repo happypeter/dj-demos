9655b1639eff9587cc79775aad221b71b2b248c7
Peter Wang <happypeter1983@gmail.com>
2017-09-25 11:11:29 +0800

    ant-navbar

--- ant-demo/src/components/Sidebar/Sidebar.js
+++ ant-demo/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import {Menu, Icon } from 'antd'
+const SubMenu = Menu.SubMenu
+
 
 const SidebarWrap = styled.div`
   display: flex;
@@ -31,13 +34,42 @@ const Username = styled.div`
   padding: 0 5px;
 `
 
+const LogoWrap = styled.div`
+  height: 64px;
+  line-height: 64px;
+  padding-left: 24px;
+  font-size: 1rem;
+  font-weight: 700;
+  background-color: #ececec;
+  flex-shrink: 0;
+`
 
 class Sidebar extends Component {
 
   render () {
     return (
       <SidebarWrap>
-        <Navbar />
+        <LogoWrap>
+          吮指后台
+        </LogoWrap>
+        <Navbar>
+         <Menu
+           style={{borderRight: 'none'}}
+           theme="light"
+           defaultOpenKeys={['dishes', 'orders']}
+           mode='inline'
+         >
+           <SubMenu key='orders' title={<span><Icon type='file' /><span>订单管理</span></span>}>
+             <Menu.Item key='/dashboard/orders'>待发货</Menu.Item>
+             <Menu.Item key='/dashboard/orders/completed'>已完成</Menu.Item>
+           </SubMenu>
+
+           <SubMenu key='dishes' title={<span><Icon type='file' /><span>甜点管理</span></span>}>
+             <Menu.Item key='/dashboard/dishes'>所有甜点</Menu.Item>
+             <Menu.Item key='/dashboard/dishes/new'>新建甜点</Menu.Item>
+           </SubMenu>
+         </Menu>
+        </Navbar>
         <LogoutArea>
           <LogoutText
             onClick={this.props.onLogout}
