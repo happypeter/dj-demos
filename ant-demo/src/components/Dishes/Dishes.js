3d0460970546f6f1b29170be85d2415e1e2b194e
Peter Wang <happypeter1983@gmail.com>
2017-09-25 16:20:20 +0800

    ant-table

--- ant-demo/src/components/Dishes/Dishes.js
+++ ant-demo/src/components/Dishes/Dishes.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react'
+import { Table } from 'antd'
+import {
+  Link
+} from 'react-router-dom'
+import styled from 'styled-components'
+
+const Poster = styled.img`
+  width: 30px;
+`
 
 class Dishes extends Component {
+
+  state = {
+    dishes:[
+      {
+          _id: '23232',
+          poster: 'https://avatars3.githubusercontent.com/u/72467?v=4&s=460',
+          name: '提拉米苏',
+          price: 32,
+          desc: '非常好吃'
+      },
+      {
+          _id: '34343',
+          poster: 'https://avatars3.githubusercontent.com/u/72467?v=4&s=460',
+          name: '黑森林',
+          price: 42,
+          desc: '来自德国'
+      }
+    ]
+  }
+
   render () {
-    console.log('xxxxxxx')
+    const columns = [
+      {
+        title: '海报',
+        dataIndex: 'poster',
+        key: 'poster',
+        render: (text) =>
+          <Poster src={text} alt='poster' />
+      },
+      {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name'
+      },
+      {
+        title: '价格',
+        dataIndex: 'price',
+        key: 'price'
+      },
+      {
+        title: '描述',
+        dataIndex: 'desc',
+        key: 'desc',
+      },
+      {
+        title: '操作',
+        render: () => (
+          <Link to=''>删除</Link>
+        )
+      }
+    ]
+    const { dishes } = this.state
     return (
-      <div className='Dishes'>
-        Dishes
+      <div className='dishes'>
+        <Table dataSource={dishes} columns={columns} />
       </div>
     )
   }
